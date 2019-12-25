c2e3d65ba53dd813ce0ff1fbee5da3d52bc859ca
Geert Gerritsen <g.gerritsen@felyx.nl>
2019-12-25 23:38:07 +0100

    Answer the som until it's correct

--- main.go
+++ main.go
@@ -11,33 +11,43 @@ import (
 )
 
 func main() {
-	log.Println("Start")
+	log.Println("Start (typ 'stop' om te stoppen)")
+	rand.Seed(time.Now().Unix())
+
 	sc := bufio.NewScanner(os.Stdin)
 
-	som, expected := generateSom()
-	log.Printf("Wat is %s?\n", som)
-	for sc.Scan() {
-		s := sc.Text()
-		answer, err := strconv.Atoi(s)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if answer == expected {
-			log.Printf("Goed gedaan!\n")
-		} else {
-			log.Printf("Helaas, volgende keer beter\n")
-		}
-
-		som, expected = generateSom()
+	outer: for {
+		som, expected := generateSom()
 		log.Printf("Wat is %s?\n", som)
+
+		for sc.Scan() {
+			if err := sc.Err(); err != nil {
+				log.Fatal(err)
+			}
+			s := sc.Text()
+
+			if s == "stop" {
+				break outer
+			}
+
+			answer, err := strconv.Atoi(s)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if answer == expected {
+				log.Printf("Goed gedaan!\n")
+				break
+			} else {
+				log.Printf("Helaas, probeer het nog eens?\n")
+			}
+		}
 	}
 
 	log.Println("Done")
 }
 
 func generateSom() (string, int) {
-	rand.Seed(time.Now().Unix())
 	a := rand.Int() % 50
 	b := rand.Int() % 50
 
